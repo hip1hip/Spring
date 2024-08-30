8e786fbb491f7e10189db478426565724603419c
hip1hip <honginpyo2100@gmail.com>
2024-08-30 18:17:15 +0900

    Spring 09 Rest-api

---
+++ rest-api-0830/src/main/java/com/example/demo/RestApi0830Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApi0830Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApi0830Application.class, args);
+	}
+
+}
