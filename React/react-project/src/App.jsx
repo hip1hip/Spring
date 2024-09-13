fbaa68ee083043fcf2541387d5061185383476c6
hip1hip <honginpyo2100@gmail.com>
2024-09-13 17:40:42 +0900

    리엑트 수업

--- React/react-project/src/App.jsx
+++ React/react-project/src/App.jsx
@@ -1,38 +1,19 @@
 import './App.css'
-// import Student from './components/Student'
-// import Counter from './components/Counter';
-// import MyComponent from './components/MyComponent';
-import CountLike from './components/CountLike'
+import Timer from './components/useEffect/Timer'
+import Counter from './components/useEffect/Counter'
+import CounterWithoutRef from './components/useRef/CounterWithoutRef'
+import CounterWithRef from './components/useRef/CounterWithRef'
+import InputWithoutRef from './components/useRef/InputWithoutRef'
+import InputWithRef from './components/useRef/InputWithRef'
+import TodoRoot from './components/contextapi/TodoRoot'
 
 function App() {
-
-  // const students = [
-  //   { name: "John", age: 20, major: 'Computer Science' },
-  //   { name: "Jane", age: 22, major: 'Mathematucs' },
-  //   { name: "Alice", age: 19, major: 'Physics' },
-  //   { name: "Bob", age: 21, major: 'Chemistry' },
-  // ];
-
   return (
     <>
-      {/* <MyComponent /> */}
-      <CountLike />
+      <TodoRoot />
 
-
-      {/* {students.map((student) =>
-        <Student
-          key={student.id}
-          {...student}
-        />
-
-      )} */}
-
-      {/* <Student {...students[0]} />
-      <Student {...students[1]} />
-      <Student {...students[2]} />
-      <Student {...students[3]} /> */}
     </>
   )
 }
 
-export default App;
+export default App
