aa94eed7751deea33adc9f6907f57f47d0c8499e
hip1hip <honginpyo2100@gmail.com>
2024-10-21 20:42:20 +0900

    김영한의 JPA 강의 05 회원 도메인 개발

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.repository;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;  // 엔티티 메니져를 만들어서 여기 주입을 하게 해줌
+    
+
+    public void save(Member member) {
+        em.persist(member);  //jap가 저장하는 역활
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+    
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name= :name" , Member.class)
+                .setParameter("name", name)
+                .getResultList();  //이름에 의해 조회 된다
+    }
+
+
+
+
+}
